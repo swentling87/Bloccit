64a51bc8b29385a421314f7669e957f1bbb29a8c
Stephen Wentling <swentling87@gmail.com>
2016-01-31 14:40:35 -0800

    bloccit assignment 7 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@
 end
 posts = Post.all
 
+Post.find_or_create_by(title: "Test Post", body: "Hello World")
+
 #Create Comments
 100.times do
   Comment.create!(
@@ -21,7 +23,7 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
-
+Comment.find_or_create_by(body: "Allons-y!")
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
