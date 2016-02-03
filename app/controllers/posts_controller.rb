e55a63d7d049066c44311a5c3ad2baf82090c727
Stephen Wentling <swentling87@gmail.com>
2016-02-02 22:34:39 -0800

    blocit-assignment-8 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      else
+        post
+      end
+    end
   end
 
   def show
