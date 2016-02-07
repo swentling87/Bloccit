90d1677cfb5eb65b121775d20103c7153ebbe26b
Stephen Wentling <swentling87@gmail.com>
2016-02-06 17:19:58 -0800

    bloccit-checkpoint-10 completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -90,4 +90,40 @@ let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData
     end
   end
 
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_post.id, post: {title: new_title, body: new_body}
+
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, id: my_post.id, post: {title: new_title, body: new_body}
+      expect(response).to redirect_to my_post
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes the post" do
+      delete :destroy, {id: my_post.id}
+
+      count = Post.where({id: my_post.id}).size
+      expect(count).to eq 0
+    end
+    it "redirects to posts index" do
+      delete :destroy, {id: my_post.id}
+
+      expect(response).to redirect_to posts_path
+    end
+  end
+
 end
