b0c7e36ef2327deaf3ca663d549e1825a7dd0c19
Stephen Wentling <swentling87@gmail.com>
2016-02-06 16:33:50 -0800

    bloccit assignment 5 update 2

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Answer, type: :model do
 
   describe "attributes" do
     it "responds to body" do
-      expect(question).to respond_to(:body)
+      expect(question.answer).to respond_to(:body)
     end
   end
 end
