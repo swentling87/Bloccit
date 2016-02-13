ee39d353927bfbb6548a4caa28d1e98511b3a446
Stephen Wentling <swentling87@gmail.com>
2016-02-13 14:17:26 -0800

    bloccit-assignment-14 completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,14 @@ class UsersController < ApplicationController
       flash.now[:alert] = "There was an error creating your account. Please try again."
       render :new
     end
+  end
+
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
 
   end
 
