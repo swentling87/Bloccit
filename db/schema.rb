a13d563f2efd89779e306ca47f177e3635be0dc9
Stephen Wentling <swentling87@gmail.com>
2016-02-15 23:23:33 -0800

    bloccit-checkpoint-17 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214030628) do
+ActiveRecord::Schema.define(version: 20160215164059) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20160214030628) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,9 +43,16 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'swentling87@gmail.com',
+admin = User.create!(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+member = User.create!(
+  name: 'Member User',
+  email: 'member@example.com',
   password: 'helloworld'
 )
 
