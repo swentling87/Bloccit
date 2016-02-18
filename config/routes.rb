1d58c361821acddb22d6bc4822b0280179edbc1b
Stephen Wentling <swentling87@gmail.com>
2016-02-17 23:02:27 -0800

    bloccit-checkpoint-19 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :labels, only: [:show]
+
   resources :topics do
     resources :posts, except: [:index]
   end
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
