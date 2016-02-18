495e08ff2061549b44ea99cc3866281af4497c7b
Stephen Wentling <swentling87@gmail.com>
2016-02-17 20:53:02 -0800

    bloccit-checkpoint-18 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.io", password: "helloworld")}
-
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
 
+  it { is_expected.to have_many(:comments) }
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user)}
   it { is_expected.to validate_presence_of(:title)}
