92988b5a0f03a4b69313d78c2352281dd6da9d04
Stephen Wentling <swentling87@gmail.com>
2016-02-20 17:53:53 -0800

    bloccit-checkpoint-20 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220181715) do
+ActiveRecord::Schema.define(version: 20160221011755) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20160220181715) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -15,6 +15,13 @@ require 'rails_helper'
          expect(response).to redirect_to(new_session_path)
        end
      end
+
+     describe "POST down_vote" do
+       it "redirects the user to the sign in view" do
+         delete :down_vote, post_id: user_post.id
+         expect(response).to redirect_to(new_session_path)
+       end
+     end
    end
 
    context "signed in user" do
@@ -52,6 +59,39 @@ require 'rails_helper'
        it ":back redirects to posts topic show" do
          request.env["HTTP_REFERER"] = topic_path(my_topic)
          post :up_vote, post_id: user_post.id
+         expect(response).to redirect_to(my_topic)
+       end
+     end
+
+   describe "POST down_vote" do
+       it "the users first vote increases number of post votes by one" do
+         votes = user_post.votes.count
+         post :down_vote, post_id: user_post.id
+         expect(user_post.votes.count).to eq(votes + 1)
+       end
+
+       it "the users second vote does not increase the number of votes" do
+         post :down_vote, post_id: user_post.id
+         votes = user_post.votes.count
+         post :down_vote, post_id: user_post.id
+         expect(user_post.votes.count).to eq(votes)
+       end
+
+       it "decreases the sum of post votes by one" do
+         points = user_post.points
+         post :down_vote, post_id: user_post.id
+         expect(user_post.points).to eq(points - 1)
+       end
+
+       it ":back redirects to posts show page" do
+         request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
+         post :down_vote, post_id: user_post.id
+         expect(response).to redirect_to([my_topic, user_post])
+       end
+
+       it ":back redirects to posts topic show" do
+         request.env["HTTP_REFERER"] = topic_path(my_topic)
+         post :down_vote, post_id: user_post.id
          expect(response).to redirect_to(my_topic)
        end
      end
