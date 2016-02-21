f04d3d0c2741db2b7b7a7429db3177a12ae8429d
Stephen Wentling <swentling87@gmail.com>
2016-02-21 14:28:07 -0800

    bloccit-assignment-21 completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,14 @@ class FavoriteMailer < ApplicationMailer
 
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+
+   def new_post(post)
+     headers["Message-ID"] = "<post/#{post.id}@immense-plains-18841.herokuapp.com>"
+     headers["In-Reply-To"] = "<post/#{post.id}@immense-plains-18841.herokuapp.com>"
+     headers["References"] = "<post/#{post.id}@immense-plains-18841.herokuapp.com>"
+
+      @post = post
+
+      mail(to: post.user.email, subject: "New post created called #{post.title}")
+    end
 end
